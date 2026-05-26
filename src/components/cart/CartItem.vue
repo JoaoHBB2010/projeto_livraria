b1b0238abb434f5bdfbdf386e894230895ccfec4
maico-edu <maico.e.passos@gmail.com>
2026-05-26 11:47:38 -0300

    Ajusta a desestruturação de props e corrige a formatação dos textos no componente CartItem

--- src/components/cart/CartItem.vue
+++ src/components/cart/CartItem.vue
@@ -5,7 +5,7 @@
 // como nome, preço e quantidade, e permite que o 
 // usuário ajuste a quantidade ou remova o item do carrinho.
 
-const props = defineProps(['item'])
+const { item } = defineProps(['item'])
 import { products } from '@/data/products'
 const emit = defineEmits([
   'incluir',
@@ -18,15 +18,19 @@ const emit = defineEmits([
 
 <template>
 
+  <div class="cart-item">
+
     <h2>{{ item.titulo }}</h2>
 
-    <p>autor: {{ item.autor }}</p>
+    <p>Autor: {{ item.autor }}</p>
 
     <p>Preço: R$ {{ item.preco }}</p>
 
-    <p>resenha: {{ item.resenha }}</p>
+    <p>Resenha: {{ item.resenha }}</p>
 
-    <p>capa: {{ item.capa }}</p>
+    <p>Capa: {{ item.capa }}</p>
+
+    <p>Categoria: {{ item.categoria }}</p>
 
     <button @click="emit('incluir', item.id)">
       +
@@ -39,8 +43,21 @@ const emit = defineEmits([
     <button @click="emit('remove', item.id)">
       remover
     </button>
+
+  </div>
+
 </template>
 
 <style scoped>
+
+.cart-item {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+button {
+  margin-right: 5px;
+}
 
 </style>
