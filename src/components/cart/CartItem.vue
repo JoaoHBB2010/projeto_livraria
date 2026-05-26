db83c9d80de4e291c596bd3bdb78e6d6fd56c19a
maico-edu <maico.e.passos@gmail.com>
2026-05-26 11:42:52 -0300

    Melhora a formatação dos comentários nos componentes do carrinho

--- src/components/cart/CartItem.vue
+++ src/components/cart/CartItem.vue
@@ -1,10 +1,44 @@
 <script setup>
-// Este arquivo é para o componente CartItem.vue, que representa um item individual no carrinho de compras. Ele exibe as informações do produto, como nome, preço e quantidade, e permite que o usuário ajuste a quantidade ou remova o item do carrinho.
+// Este arquivo é para o componente CartItem.vue, 
+// que representa um item individual no carrinho 
+// de compras. Ele exibe as informações do produto, 
+// como nome, preço e quantidade, e permite que o 
+// usuário ajuste a quantidade ou remova o item do carrinho.
+
+const props = defineProps(['item'])
+import { products } from '@/data/products'
+const emit = defineEmits([
+  'incluir',
+  'retirar',
+  'remove'
+])
+
 
 </script>
 
 <template>
 
+    <h2>{{ item.titulo }}</h2>
+
+    <p>autor: {{ item.autor }}</p>
+
+    <p>Preço: R$ {{ item.preco }}</p>
+
+    <p>resenha: {{ item.resenha }}</p>
+
+    <p>capa: {{ item.capa }}</p>
+
+    <button @click="emit('incluir', item.id)">
+      +
+    </button>
+
+    <button @click="emit('retirar', item.id)">
+      -
+    </button>
+
+    <button @click="emit('remove', item.id)">
+      remover
+    </button>
 </template>
 
 <style scoped>
--- src/components/cart/CartSummary.vue
+++ src/components/cart/CartSummary.vue
@@ -1,5 +1,7 @@
 <script setup>
-// Este arquivo é um componente Vue que representa o último resumo do carrinho de compras, exibindo o total geral e um botão para finalizar a compra.
+// Este arquivo é um componente Vue que representa o último 
+// resumo do carrinho de compras, exibindo o total geral e um 
+// botão para finalizar a compra.
 
 </script>
 
