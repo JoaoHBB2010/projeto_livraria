c23185a181631c749855e1d2a266e990aef0b3b1
JoaoHBB2010 <joaohenriquebatista.2010@gmail.com>
2026-05-29 15:04:06 -0300

    Inicio do Footer

---
+++ src/components/layout/AppFooter.vue
@@ -0,0 +1,35 @@
+<script setup>
+</script>
+
+<template>
+  <footer>
+    <div>
+        <h1>
+            <RouterLink><img src="/public/logoDefinitiva.png" alt="LOGO"></RouterLink>
+        </h1>
+    </div>
+
+    <a href="#">VOLTAR AO TOPO</a>
+  </footer>
+</template>
+
+<style scoped>
+footer{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    border-top: 2px solid #0ba720;
+    background: #ffffff;
+    color: #0ba720;
+    margin-top: auto;
+    display: flex;
+    align-items: center;
+}
+footer a{
+    text-decoration: none;
+}
+footer img{
+    max-width: 15%;
+}
+</style>
